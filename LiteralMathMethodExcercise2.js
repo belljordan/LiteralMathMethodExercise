6fede615b0299c0a722b202051ec305185faf7f0
Jordan Bell <jordanbell@MacBook-Pro-100.local>
2020-10-14 14:00:08 -0500

    Created

---
+++ LiteralMathMethodExcercise2.js
@@ -0,0 +1,26 @@
+let warmHugs = "Hi,I'm Olaf and I like warm hugs";
+console.log(warmHugs.toUpperCase());
+warmHugs = warmHugs.replace("like","love");
+// console.log(warmHugs);
+let beenImpaled = "Oh,look at that. I've been impaled.";
+console.log(beenImpaled.slice(-18));
+
+let dotDotDot = "...";
+console.log(dotDotDot);
+let skullBones = `I Don't have a Skull ${dotDotDot} or bones`;
+console.log(skullBones);
+
+console.log(Math.PI); 
+
+let RandomNumber = Math.random ();
+RandomNumber *= 3; 
+RandomNumber = Math.ceil(RandomNumber);
+console.log(RandomNumber);
+
+console.log("let It Go! ".toUpperCase().repeat(2));
+
+let TurtleButts = "Bright side? Turtles can breathe through their butts";
+console.log(TurtleButts.replace(/ /g, "_"));
+
+console.log(Math.sqrt(2));
+
---
+++ LiteralMathMethodExercise.js
@@ -0,0 +1,5 @@
+var warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs); 
+
+var beenImpaled = "oh,look at that.I've been impaled.";
+console.log(beenImpaled);
